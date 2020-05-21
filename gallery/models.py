77af3a5ce66b8f55c1b73a49a52949c2ef28a597
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 21:02:32 +0300

    imported models in my tests

--- gallery/models.py
+++ gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from _datetime import datetime
 
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
@@ -9,9 +10,17 @@ class Editor(models.Model):
         return self.first_name
     class Meta:
         ordering = ['first_name']
-        
+
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(default=datetime.now,blank=True)
