bf105b616abf519a8a7d89f41c6d412153b2f8d6
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 21:35:44 +0300

    added a phone_number Character field to our Editor model

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,7 @@ class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
     
     def __str__(self):
         return self.first_name
@@ -14,7 +15,7 @@ class Editor(models.Model):
 
     def delete_editor(self):
         self.delete() 
-            
+
     class Meta:
         ordering = ['first_name']
 
