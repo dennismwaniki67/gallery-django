b90705ece3dc54fba91acc7d0b0367b30c3607c8
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 16:20:48 +0300

    create new regex pattern to match the dates

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.gallery_of_day,name='galleryToday')
-]
+    url('^today/$',views.gallery_of_day,name='galleryToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery') ] 
