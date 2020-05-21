676ded88f839855468ed21db2ad4bbcd0248c3e4
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 17:14:52 +0300

    defined 'bootstrap3' which connects the django-bootstrap dependency to our project and any application defined in our project.

--- gallery/views.py
+++ gallery/views.py
@@ -5,46 +5,27 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
-    
+
 def gallery_of_day(request):
     date = dt.date.today()
+    return render(request, 'gallery/today-gallery.html')
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
-def convert_dates(dates):
-
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-def past_days_galley(request,past_date):
-
+def past_days_gallery(request, past_date):
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
+        assert False
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(gallery_today)
+
+    gallery = Article.days_gallery(date)
+    return render(request, 'gallery/past-gallery.html', {"date": date,"gallery":gallery})
+    
+def gallery_today(request):
+    date = dt.date.today()
+    gallery = Article.todays_gallery()
+    return render(request, 'gallery/today-gallery.html', {"date": date,"gallery":gallery})
