8765a08a9c60be1f058a7b784c7d9c3b2fa72338
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 21:11:53 +0300

    created tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,17 @@ from django.test import TestCase
 from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.dennis= Editor(first_name = 'Dennis', last_name ='Mwaniki', email ='dennismwaniki67@gmail.com')
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.dennis,Editor))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.dennis.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
