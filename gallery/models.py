aaa43b8c2e686399ed21dbf64cff278c3be295a6
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-21 21:13:57 +0300

    created save method

--- gallery/models.py
+++ gallery/models.py
@@ -8,6 +8,8 @@ class Editor(models.Model):
     
     def __str__(self):
         return self.first_name
+    def save_editor(self):
+        self.save()   
     class Meta:
         ordering = ['first_name']
 
